5baeb10b5e59aa0aaa7ceaa9cc93bec9125135fe
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-10-18 15:28:36 -0300

    CreateProductService

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError(`There is already a product with this name ${name}`);
+    }
+
+    const product = productRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
