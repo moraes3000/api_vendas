4900aa5bf8f576d8751364cae3c2a258637b6847
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-10-19 13:07:19 -0300

    add jwtoken

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 
 import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepositories';
@@ -11,8 +12,13 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -26,7 +32,15 @@ class CreateSessionService {
       throw new AppError(`Incorrect email or password`, 401);
     }
 
-    return user;
+    const token = sign({}, 'e87ae5d1eae36d7ccc3170dd0173df40', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
