c2857a515d0f5f6695678e98dde259e65f540a9c
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-10-19 17:55:39 -0300

    Entidade de Tokens de Usuários

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user_tokens')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
