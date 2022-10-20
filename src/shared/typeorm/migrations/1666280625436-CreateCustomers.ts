dffcac0e3d3696f274ac9bf9bf0f525bc131d49b
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-10-20 12:45:39 -0300

    Migração da Tabela customers

---
+++ src/shared/typeorm/migrations/1666280625436-CreateCustomers.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCustomers1666280625436 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('customers');
+  }
+}
