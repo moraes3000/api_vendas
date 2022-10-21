6cfaa6827719f97a3f4541356a0d0999d8881f12
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-10-21 10:25:35 -0300

    Entidade de Produtos em um Pedido de Compras

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,15 +1,20 @@
+import OrdersProducts from '@modules/orders/typeorm/entities/OrdersProducts';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity('products')
 class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => OrdersProducts, order_product => order_product.product)
+  order_product: OrdersProducts[];
 
   @Column()
   name: string;
